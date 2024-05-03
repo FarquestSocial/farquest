1e5dd7bafa74028c50714001c9fbc972bc3e9fea
Kainoa Kanter <kainoa@t1c.dev>
2024-05-03 16:27:01 -0700

    feat: swagger

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(swagger())
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
