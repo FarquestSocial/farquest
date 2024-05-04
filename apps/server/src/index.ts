5c28e0c62db51e8be707f2b28455600fc292a0f8
Kainoa Kanter <kainoa@t1c.dev>
2024-05-03 21:22:46 -0700

    routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,7 +4,7 @@ import { Logestic } from "logestic";
 import { privy } from "./privy";
 import { customAlphabet, urlAlphabet } from "nanoid";
 import type { AuthTokenClaims } from "@privy-io/server-auth";
-// import { }
+import { services } from "services";
 
 async function doAuth(headers: Record<string, string | undefined>) {
 	if (!headers.authorization?.startsWith("Bearer ")) {
@@ -29,6 +29,30 @@ const app = new Elysia()
 	.use(swagger())
 	.use(Logestic.preset("common"))
 	.get("/", () => "Hello Elysia")
+	// TODO: make work
+	.post("/organizations/create", async ({ headers }) => {
+		const orginizationId = await services.organizationService.createOrganization(
+			"Farquest",
+			"https://localhost:5173/auth",
+			"https://localhost:5173/auth/callback",
+		);
+		return {
+			id: orginizationId,
+		};
+	})
+	.guard({
+		beforeHandle({ set, headers }) {
+			const header = headers["FARQUEST-API-KEY"];
+			if (!header) {
+				return (set.status = 'Unauthorized');
+			}
+			const userApiKey = services.organizationService.getOrganizationIdByApiKey(
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				headers["FARQUEST-API-KEY"]!,
+			);
+			return true;
+		},
+	})
 	.post(
 		"/session/:correlatedId",
 		async ({ headers, params }) => {
@@ -46,16 +70,24 @@ const app = new Elysia()
 				return error(401);
 			}
 			const user = await privy.getUser(verifiedClaims.userId);
-
+			const orginizationId =
+				services.organizationService.getOrganizationIdByApiKey();
+			await services.userService.createUser(
+				verifiedClaims.userId,
+				user.orginizationId,
+				user.farcaster?.fid,
+			);
 			const sessionToken = customAlphabet(urlAlphabet, 20);
 			// TODO: store session token in redis
 			return {
 				redirectUrl: `https://localhost:5173/auth?state=${sessionToken}`,
 			};
 		},
-		// {
-		// 	body: t.Object({}),
-		// },
+		{
+			headers: t.Object({
+				authorization: t.String(),
+			}),
+		},
 	)
 	.listen(3000);
 
