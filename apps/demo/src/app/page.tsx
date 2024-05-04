dfa0b364e1ce799bee1cfc4284e69c06c3af5968
anthony-urbina <aurbinava002@gmail.com>
2024-05-03 17:43:30 -0700

    saving progress

--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -1,3 +1,8 @@
+import { treaty } from "@elysiajs/eden";
+import { App } from "../../../server/src";
+
+const client = treaty<App>("localhost:3000");
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
