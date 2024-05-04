4527205282f1a8c08dcd7d8fd96ba41a0c9f9690
anthony-urbina <aurbinava002@gmail.com>
2024-05-04 13:58:56 -0700

    admin panel demo app ready

--- apps/demo/src/app/components/admin/modal/modal-wrapper.tsx
+++ apps/demo/src/app/components/admin/modal/modal-wrapper.tsx
@@ -2,97 +2,89 @@ import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, type ReactNode } from "react";
 
 export const ModalWrapper = ({
-  isOpen,
-  onClose,
-  children,
-  className,
+	isOpen,
+	onClose,
+	children,
+	className,
 }: {
-  isOpen: boolean;
-  onClose: () => void;
-  children: ReactNode;
-  className?: string;
+	isOpen: boolean;
+	onClose: () => void;
+	children: ReactNode;
+	className?: string;
 }) => {
-  return (
-    <Transition
-      appear
-      show={isOpen}
-      as={Fragment}
-    >
-      <Dialog
-        as='div'
-        className='relative z-50'
-        onClose={onClose}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
-          <div className='fixed inset-0 z-50 bg-black opacity-90' />
-        </Transition.Child>
+	return (
+		<Transition appear show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-50" onClose={onClose}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="fixed inset-0 z-50 bg-black opacity-90" />
+				</Transition.Child>
 
-        <div className='fixed inset-0 z-50 '>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
-            <Transition.Child
-              as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
-            >
-              <Dialog.Panel
-                className={`relative w-11/12 max-w-3xl transform rounded-lg  bg-secondary backdrop-blur-lg p-0 text-left align-middle shadow-xl transition-all ${
-                  className ?? ""
-                }`}
-              >
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
+				<div className="fixed inset-0 z-50 text-text">
+					<div className="flex min-h-full items-center justify-center p-4 text-center">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 scale-95"
+							enterTo="opacity-100 scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 scale-100"
+							leaveTo="opacity-0 scale-95"
+						>
+							<Dialog.Panel
+								className={`relative w-11/12 max-w-3xl transform rounded-lg bg-secondary backdrop-blur-lg p-0 text-left align-middle shadow-xl transition-all ${
+									className ?? ""
+								}`}
+							>
+								{children}
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition>
+	);
 };
 
 interface ModalHeaderProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const ModalHeader = ({ children }: ModalHeaderProps) => {
-  return (
-    <header className='flex items-center justify-between border-b p-5'>
-      {children}
-    </header>
-  );
+	return (
+		<header className="flex items-center justify-between border-b p-5">
+			{children}
+		</header>
+	);
 };
 
 interface ModalBodyProps {
-  children: React.ReactNode;
-  className?: string;
+	children: React.ReactNode;
+	className?: string;
 }
 
 export const ModalBody = ({ children, className }: ModalBodyProps) => {
-  return (
-    <main
-      id='modal-body'
-      className={`max-h-[70dvh] min-h-full  px-32 py-5 ${className ?? ""}`}
-    >
-      {children}
-    </main>
-  );
+	return (
+		<main
+			id="modal-body"
+			className={`max-h-[70dvh] min-h-full  px-32 py-5 ${className ?? ""}`}
+		>
+			{children}
+		</main>
+	);
 };
 
 interface ModalFooterProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export const ModalFooter = ({ children }: ModalFooterProps) => {
-  return <footer className='flex w-full justify-end p-5'>{children}</footer>;
+	return <footer className="flex w-full justify-end p-5">{children}</footer>;
 };
--- apps/demo/src/app/layout.tsx
+++ apps/demo/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { Sidebar } from "./components/admin/sidebar/Sidebar";
-import { FilterBar } from "./components/admin/filters/FilterBar";
-import { AdminWrapper } from "@/contexts/admin-context";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -15,18 +15,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="text-text">
-			<AdminWrapper>
-				<body className={"p-24 flex gap-x-4 container mx-auto"}>
-					<div className="basis-2/6 h-full">
-						<Sidebar />
-					</div>
-					<div className="w-full gap-y-4 flex flex-col">
-						<FilterBar />
-						<div className="w-full rounded-2xl">{children}</div>
-					</div>
-				</body>
-			</AdminWrapper>
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
