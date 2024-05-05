0e1615eefb3d5b4377e936335e4c7910670a08f6
solidityio <admin@Andress-MacBook-Pro.local>
2024-05-04 22:17:32 -0400

    route types

--- apps/demo/src/constants/routes.ts
+++ apps/demo/src/constants/routes.ts
@@ -1,83 +1,9 @@
-import type { FilterType } from "@/utils/types";
-import { BASE_URL } from "./base-url";
-
 export const routes = {
-	twitterAuth: `${BASE_URL}/twitter/auth`,
-	getMe: `${BASE_URL}/user/me`,
-	getUser: `${BASE_URL}/user/profile`,
-	getPosition: `${BASE_URL}/user/position`,
-	getNonce: `${BASE_URL}/user/nonce`,
-	getMessageToSign: (walletAddress: string) => {
-		const queryParams = new URLSearchParams();
-
-		if (!walletAddress) throw new Error("Wallet Address is required");
-
-		queryParams.append("walletAddress", walletAddress);
-
-		return `${BASE_URL}/user/wallet-address-challenge?${queryParams.toString()}`;
-	},
-	updateWalletAddress: `${BASE_URL}/user/wallet-address`,
-	updateUser: `${BASE_URL}/user/update`,
-	validateUsername: `${BASE_URL}/user/validate-username`,
-	getFeaturedQuest: `${BASE_URL}/quests/featured`,
-	getQuestById: (id: string) => {
-		return `${BASE_URL}/quests/${id}`;
-	},
-	getQuests: ({
-		page,
-		take,
-		filter,
-	}: {
-		page: number;
-		take: number;
-		filter: FilterType;
-	}) => {
-		const queryParams = new URLSearchParams();
-
-		if (page !== undefined) {
-			queryParams.append("page", page.toString());
-		}
-		if (take !== undefined) {
-			queryParams.append("take", take.toString());
-		}
-		if (filter !== undefined) {
-			queryParams.append("filter", filter);
-		}
-
-		return `${BASE_URL}/quests?${queryParams.toString()}`;
-	},
-	completeQuest: ({
-		questId,
-		token,
-		tweetUrl,
-		userAnswerId,
-		email,
-	}: {
-		questId: string;
-		token: string;
-		tweetUrl?: string;
-		userAnswerId?: string;
-		email?: string;
-	}) => {
-		const params = new URLSearchParams();
-		if (!questId) throw new Error("Quest ID is required");
-		if (!token) throw new Error("Token is required");
-		if (tweetUrl) {
-			params.append("tweetUrl", tweetUrl);
-		}
-		if (userAnswerId) {
-			params.append("userAnswerId", userAnswerId);
-		}
-
-		if (email) {
-			params.append("email", email);
-		}
-
-		params.append("token", token);
-
-		return `${BASE_URL}/completion/${questId}/server-sent?${params.toString()}`;
-	},
-	disconnectTelegram: `${BASE_URL}/user/disconnect-telegram`,
-	disconnectDiscord: `${BASE_URL}/user/disconnect-discord`,
-	connectDiscord: `${BASE_URL}/discord/auth`,
+	completeQuest: `api/complete-quest`,// POST
+	createQuest: `api/create-quest`,// POST
+	createSession: `api/create-session`,// POST
+	getAllQuestTypes: `api/get-all-quest-types`,// GET: IQuestType[]
+	getAllRequiredFields: `api/get-all-required-fields`,// GET: IQuestTypeFieldsResponse
+	getAllQuests: `api/get-all-quests`,// GET: IQuest[]
+	getQuestById: `api/get-quest-by-id`,// GET: IQuestResponse
 };
--- packages/services/src/completion/completion.repository.ts
+++ packages/services/src/completion/completion.repository.ts
@@ -2,150 +2,153 @@ import { plainToInstance } from "class-transformer";
 import type { PrismaClient } from "database";
 //auto like retweet and post stretch goal
 export class CompletionRepository {
-	constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaClient) {}
 
-	async canUserCompleteQuest(userId: string, questId: string) {
-		const fid = await this.getUserFid(userId);
-		if (!fid) {
-			return false;
-		}
-		const hasCompleted = await this.hasCompletedQuest(userId, questId);
-		if (hasCompleted) {
-			return false;
-		}
-		return true;
-	}
+  async canUserCompleteQuest(
+    userId: string,
+    questId: string
+  ): Promise<boolean> {
+    const fid = await this.getUserFid(userId);
+    if (!fid) {
+      return false;
+    }
+    const hasCompleted = await this.hasCompletedQuest(userId, questId);
+    if (hasCompleted) {
+      return false;
+    }
+    return true;
+  }
 
-	getCorrelationIdFromUserId = async (userId: string) => {
-		const $ = await this.prisma.user.findUnique({
-			where: {
-				id: userId,
-			},
-			select: {
-				correlationId: true,
-			},
-		});
-		if (!$) {
-			throw new Error("User not found");
-		}
-		return $.correlationId;
-	};
+  getCorrelationIdFromUserId = async (userId: string) => {
+    const $ = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        correlationId: true,
+      },
+    });
+    if (!$) {
+      throw new Error("User not found");
+    }
+    return $.correlationId;
+  };
 
-	getCallBackUrlAndCallBackMetadataForQuest = async (questId: string) => {
-		const $ = await this.prisma.quests.findUnique({
-			where: {
-				id: questId,
-			},
-			select: {
-				customCallbackMetadata: true,
-				organization: {
-					select: {
-						callbackUrl: true,
-					},
-				},
-			},
-		});
-		if (!$) {
-			throw new Error("Quest not found");
-		}
-		return {
-			callback: $.organization.callbackUrl,
-			metadata: $.customCallbackMetadata,
-		};
-	};
+  getCallBackUrlAndCallBackMetadataForQuest = async (questId: string) => {
+    const $ = await this.prisma.quests.findUnique({
+      where: {
+        id: questId,
+      },
+      select: {
+        customCallbackMetadata: true,
+        organization: {
+          select: {
+            callbackUrl: true,
+          },
+        },
+      },
+    });
+    if (!$) {
+      throw new Error("Quest not found");
+    }
+    return {
+      callback: $.organization.callbackUrl,
+      metadata: $.customCallbackMetadata,
+    };
+  };
 
-	getUserWalletAddress = async (userId: string) => {
-		const user = await this.prisma.user.findUnique({
-			where: {
-				id: userId,
-			},
-			select: {
-				ethAddress: true,
-			},
-		});
-		if (!user) {
-			throw new Error("User not found");
-		}
-		return user.ethAddress;
-	};
+  getUserWalletAddress = async (userId: string) => {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        ethAddress: true,
+      },
+    });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user.ethAddress;
+  };
 
-	getQuestValidationCriteria = async <T>(
-		questId: string,
-		type: new () => T,
-	): Promise<T> => {
-		const criteria = await this.prisma.quests
-			.findUniqueOrThrow({
-				where: {
-					id: questId,
-				},
-				select: {
-					validationCriteria: true,
-				},
-			})
-			.catch((e) => {
-				throw new Error("Quest not found");
-			});
-		if (!criteria.validationCriteria) {
-			throw new Error("Quest does not have validation criteria");
-		}
-		return plainToInstance(type, criteria.validationCriteria);
-	};
-	//bad dont do this
-	async getUserFid(userId: string) {
-		const $ = await this.prisma.user.findFirst({
-			where: {
-				id: userId,
-			},
-			select: {
-				fid: true,
-			},
-		});
-		if (!$) {
-			throw new Error("User not found");
-		}
-		return $.fid;
-	}
-	async getQuestType(questId: string) {
-		const data = await this.prisma.quests.findUnique({
-			where: {
-				id: questId,
-			},
-			select: {
-				questType: {
-					select: {
-						type: true,
-					},
-				},
-			},
-		});
-		if (!data) {
-			throw new Error("Quest not found");
-		}
-		return data.questType.type;
-	}
-	async hasCompletedQuest(userId: string, questId: string) {
-		const hasCompleted = await this.prisma.userQuestCompletion.findUnique({
-			where: {
-				userId_questId: {
-					userId: userId,
-					questId: questId,
-				},
-			},
-			select: {
-				id: true,
-			},
-		});
-		if (hasCompleted) {
-			return true;
-		}
-		return false;
-	}
-	async completeQuest(userId: string, questId: string) {
-		return this.prisma.userQuestCompletion.create({
-			data: {
-				userId: userId,
-				questId: questId,
-			},
-		});
-	}
+  getQuestValidationCriteria = async <T>(
+    questId: string,
+    type: new () => T
+  ): Promise<T> => {
+    const criteria = await this.prisma.quests
+      .findUniqueOrThrow({
+        where: {
+          id: questId,
+        },
+        select: {
+          validationCriteria: true,
+        },
+      })
+      .catch((e) => {
+        throw new Error("Quest not found");
+      });
+    if (!criteria.validationCriteria) {
+      throw new Error("Quest does not have validation criteria");
+    }
+    return plainToInstance(type, criteria.validationCriteria);
+  };
+  //bad dont do this
+  async getUserFid(userId: string) {
+    const $ = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+      select: {
+        fid: true,
+      },
+    });
+    if (!$) {
+      throw new Error("User not found");
+    }
+    return $.fid;
+  }
+  async getQuestType(questId: string) {
+    const data = await this.prisma.quests.findUnique({
+      where: {
+        id: questId,
+      },
+      select: {
+        questType: {
+          select: {
+            type: true,
+          },
+        },
+      },
+    });
+    if (!data) {
+      throw new Error("Quest not found");
+    }
+    return data.questType.type;
+  }
+  async hasCompletedQuest(userId: string, questId: string) {
+    const hasCompleted = await this.prisma.userQuestCompletion.findUnique({
+      where: {
+        userId_questId: {
+          userId: userId,
+          questId: questId,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (hasCompleted) {
+      return true;
+    }
+    return false;
+  }
+  async completeQuest(userId: string, questId: string) {
+    return this.prisma.userQuestCompletion.create({
+      data: {
+        userId: userId,
+        questId: questId,
+      },
+    });
+  }
 }
